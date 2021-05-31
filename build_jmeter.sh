48b2ef0dbeccdc53859d7256abb2bea749fa4d40
Adalberto Junior <adalrsjr1@gmail.com>
2021-05-31 18:16:38 -0400

    fix issues in daytrader manifests due to max_db_connection constraint

--- build_jmeter.sh
+++ build_jmeter.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 docker build -f Dockerfile-jmeter -t smarttuning/jmeter_daytrader .
+docker build -f Dockerfile-dynamic_jmeter -t smarttuning/dynamic_jmeter_daytrader .
 
 docker push smarttuning/jmeter_daytrader
+docker push smarttuning/dynamic_jmeter_daytrader
